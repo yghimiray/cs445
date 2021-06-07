51eac8b92fee505b4324e2f9c14a271da67e769a
Yogesh <yghimiray@miu.edu>
2021-06-06 19:52:49 -0500

    fibonacci

--- lab10/exe04.js
+++ lab10/exe04.js
@@ -1,8 +1,31 @@
 
-// function fibonacci(n) {
-//     if (n <= 1) {
-//         return 1
-//     }
-//     return fibonacci(n - 1) + fibonacci(n - 2);
-// }
+function fibonacci1(n) {
+    if (n < 1) {
+        return 0;
+    }
+    if(n===1){
+        return 1;
+    }
+    return fibonacci1(n - 1) + fibonacci1(n - 2);
+}
 
+function fibonacci(num){
+    let object = {};
+    let value;
+    function fibonacciHelper(num){
+        if(object[num]){
+            value = object[num];
+        }else if (num === 0 || num === 1){
+            value = num ;
+            object[num] = value;
+        }else {
+            value = fibonacciHelper(num - 1) + fibonacciHelper(num - 2);
+            object[num] = value;
+        }
+        return value;
+    }
+    return fibonacciHelper(num);
+}
+//0,1,1,2,3,5,8,13,21,34,55,89
+console.log(fibonacci(40));
+// console.log(fibonacci1(40));
